5a92a09de3c57401557eed31e7d457277ef864ff
0zzysome <oscar.rosin@hotmail.com>
2022-10-18 13:28:37 +0200

    bfkhbakf

---
+++ JsonUppgift/Enemy.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Enemy
+{
+    public string Name { get; set; }
+    public int Hp { get; set; }
+    public int Dmg { get; set; }
+
+}
---
+++ JsonUppgift/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Text.Json;
+
+Enemy wisp = new Enemy();
+
+Console.WriteLine("give it a name");
+wisp.Name = Console.ReadLine();
+
+Console.WriteLine("How much hp shuld it have");
+int HpInput;
+int.TryParse(Console.ReadLine(), out HpInput);
+wisp.Hp = HpInput;
+
+Console.WriteLine("give it a name");
+int DmgInput;
+int.TryParse(Console.ReadLine(), out DmgInput);
+wisp.Dmg = DmgInput;
+
+Console.ReadLine();
