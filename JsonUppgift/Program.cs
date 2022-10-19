e93038fbbfae93fb25521ce4ad81e73d2f7d5ccd
0zzysome <oscar.rosin@hotmail.com>
2022-10-19 12:32:25 +0200

    jfahjfv

--- JsonUppgift/Program.cs
+++ JsonUppgift/Program.cs
@@ -6,14 +6,16 @@ Enemy wisp = new Enemy();
 Console.WriteLine("give it a name");
 wisp.Name = Console.ReadLine();
 
-Console.WriteLine("How much hp shuld it have");
+Console.WriteLine("How much hp shuld it have?");
 int HpInput;
 int.TryParse(Console.ReadLine(), out HpInput);
 wisp.Hp = HpInput;
 
-Console.WriteLine("give it a name");
+Console.WriteLine("How much damage should it do?");
 int DmgInput;
 int.TryParse(Console.ReadLine(), out DmgInput);
 wisp.Dmg = DmgInput;
 
+string json = JsonSerializer.Serialize<Enemy>(wisp);
+File.WriteAllText("enemy.json", json);
 Console.ReadLine();
